7889bde7a98e3482c43a8df7a586a813f8275651
yashashree-naik-01 <yashashree.naik.01@gmail.com>
2025-12-05 15:22:15 +0530

    Add files via upload

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const loginRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 5 requests per windowMs
+  max: 1000, // Limit each IP to 5 requests per windowMs
   message: { error: 'Too many login attempts, please try again later' },
   standardHeaders: true,
   legacyHeaders: false,
